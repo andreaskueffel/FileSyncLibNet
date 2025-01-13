48c48a93fb13fa8087effea7f21a09c8025d9bb8
Andreas Kueffel <andreas.kueffel@praewema.de>
2025-01-13 12:40:00 +0100

    include Version in buildcommand

--- FileSyncAppWin/buildcommand.cmd
+++ FileSyncAppWin/buildcommand.cmd
@@ -1,1 +1,1 @@
-dotnet publish -o pub -c Release -p:PublishSingleFile=true -p:PublishTrimmed=false -r win-x86 --self-contained .\FileSyncAppWin.csproj
+dotnet publish -o pub -c Release -p:PublishSingleFile=true -p:PublishTrimmed=false -p:Version=0.0.16 -r win-x86 --self-contained .\FileSyncAppWin.csproj
