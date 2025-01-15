a0e538245d4c83c8df3b719c961894d3cf862f42
Andreas Kueffel <andreas.kueffel@praewema.de>
2025-01-15 13:46:04 +0100

    implement clean job handling in AbstractProvider

--- FileSyncLibNet/SyncProviders/AbstractProvider.cs
+++ FileSyncLibNet/SyncProviders/AbstractProvider.cs
@@ -1,5 +1,6 @@
 ﻿using FileSyncLibNet.AccessProviders;
 using FileSyncLibNet.Commons;
+using FileSyncLibNet.FileCleanJob;
 using FileSyncLibNet.FileSyncJob;
 using Microsoft.Extensions.Logging;
 using System;
@@ -14,32 +15,77 @@ namespace FileSyncLibNet.SyncProviders
 
         public AbstractProvider(IFileJobOptions jobOptions) : base(jobOptions)
         {
-            if (!(JobOptions is IFileSyncJobOptions syncJobOptions))
-                throw new ArgumentException("this instance has no information about syncing files, it has type " + JobOptions.GetType().ToString());
-            if (syncJobOptions.SourcePath.StartsWith("scp:"))
+            if (!(JobOptions is IFileCleanJobOptions) && !(JobOptions is IFileSyncJobOptions))
             {
-                SourceAccess = new ScpAccessProvider(syncJobOptions.Credentials, jobOptions.Logger, stateFilename: null);
+                throw new ArgumentException("this instance has no information about syncing files or cleaning files, it has type " + JobOptions.GetType().ToString());
+            }
+            string stateFilename = null;
+            if (JobOptions is IFileSyncJobOptions syncJobOptions)
+            {
+                if (syncJobOptions.SourcePath.StartsWith("scp:"))
+                {
+                    SourceAccess = new ScpAccessProvider(syncJobOptions.Credentials, JobOptions.Logger, stateFilename: null);
+                }
+                else
+                {
+                    SourceAccess = new FileIoAccessProvider(JobOptions.Logger, stateFilename: null);
+                }
+                SourceAccess.UpdateAccessPath(syncJobOptions.SourcePath);
+                stateFilename = null;
+            }
+            if (JobOptions.DestinationPath.StartsWith("scp:"))
+            {
+                DestinationAccess = new ScpAccessProvider(JobOptions.Credentials, JobOptions.Logger, stateFilename);
             }
             else
             {
-                SourceAccess = new FileIoAccessProvider(jobOptions.Logger, stateFilename: null);
-            }
-            SourceAccess.UpdateAccessPath(syncJobOptions.SourcePath);
-            string stateFilename = syncJobOptions.RememberRemoteState ? syncJobOptions.GetHashedName() : null;
-            if (syncJobOptions.DestinationPath.StartsWith("scp:"))
-            {
-                DestinationAccess = new ScpAccessProvider(syncJobOptions.Credentials, jobOptions.Logger, stateFilename);
-            }
-            else
-            {
-                DestinationAccess = new FileIoAccessProvider(jobOptions.Logger, stateFilename);
+                DestinationAccess = new FileIoAccessProvider(JobOptions.Logger, stateFilename);
             }
 
         }
 
         public override void DeleteFiles()
         {
-            throw new NotImplementedException();
+            //Use from file io provider and adapt to use access provider
+
+            if (!(JobOptions is IFileCleanJobOptions jobOptions))
+                throw new ArgumentException("this instance has no information about deleting files, it has type " + JobOptions.GetType().ToString());
+            try
+            {
+                string formattedDestinationPath = string.Format(jobOptions.DestinationPath, DateTime.Now);
+                DestinationAccess.UpdateAccessPath(formattedDestinationPath);
+            }
+            catch (Exception ex) { logger?.LogError(ex, "exception formatting destination accesspath with DateTime.Now as 0 arg {A}", jobOptions.DestinationPath); }
+
+            try
+            {
+                var minimumLastWriteTime = DateTimeOffset.Now - jobOptions.MaxAge;
+                var sourceFiles = DestinationAccess.GetFiles(minimumLastWriteTime: minimumLastWriteTime.DateTime,
+                                                       pattern: JobOptions.SearchPattern,
+                                                       recursive: JobOptions.Recursive,
+                                                       subfolders: JobOptions.Subfolders,
+                                                       olderFiles: true);
+                int fileCount = 0;
+                foreach (var fileToDelete in sourceFiles)
+                {
+                    logger.LogDebug("deleting file {A}", fileToDelete.Name);
+
+                    try
+                    {
+                        DestinationAccess.Delete(fileToDelete);
+
+                        fileCount++;
+                    }
+                    catch (Exception ex) { logger.LogError(ex, "exception deleting file {A}", fileToDelete.Name); }
+                    logger.LogDebug("deleted {A} files", fileCount);
+                }
+
+
+            }
+            catch (Exception exc)
+            {
+                logger.LogError(exc, "TimerCleanup_Tick threw an exception");
+            }
         }
 
         public override void SyncSourceToDest()
